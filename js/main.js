daac89a09760f733506d03feecf424485d25de26
Rohkage <rohan29varma@gmail.com>
2024-01-31 19:05:17 +0530

    siddharth branch resolved

--- js/main.js
+++ js/main.js
@@ -20,7 +20,7 @@ $(document).ready(function(){
     });
 
     const counters = document.querySelectorAll('.counter');
-    const speed = 120;
+    const speed = 80;
     counters.forEach(counter => {
 	const updateCount = () => {
 		const target = +counter.getAttribute('data-target');
